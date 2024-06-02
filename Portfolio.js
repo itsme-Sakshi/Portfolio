671c8d936d0fa2767e78075324284fd40e45e9c1
itsme-Sakshi <93009332+itsme-Sakshi@users.noreply.github.com>
2024-06-02 11:47:48 +0530

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,18 @@
+
+// let navBar=document.querySelector(".navbar");
+// let navItems=document.querySelector(".nav-items");
+
+// let newimg=document.createElement("img");
+// img.src="./images/download.png";
+
+// var x=window.matchMedia("(max-width:639px)");
+// if(x.matches){
+//     navBar.appendChild(img);
+//     document.navItems.style.visibility="hidden";
+// }  
+
+// // document.navItems.style.visibility="hidden";
+
+// // document.nav-items.style.back = "red";
+// // navBar.classList.add("hamburg");
+// // // .remove
